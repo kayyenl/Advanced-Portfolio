61a5de240c631cc164e1701ab1a313f0c1b0ede6
kayyenl <yenyenlee9@gmail.com>
2022-07-11 00:13:41 +0800

    done with the email portion. cant wait to close the modal

---
+++ index.js
@@ -0,0 +1,25 @@
+// D8M9j6zNzNEXr2wi1
+// service_xdfvfdj
+// template_w2pk8wr
+
+function contact(event) {
+    event.preventDefault()
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += " modal__overlay--visible"
+    emailjs
+        .sendForm(
+            'service_xdfvfdj',
+            'template_w2pk8wr',
+            event.target,
+            'D8M9j6zNzNEXr2wi1'
+        ).then( () => { //for the console log to be printed after the server, it needs to be in a THEN.
+            loading.classList.remove("modal__overlay--visible")
+            success.classList += " modal__overlay--visible"
+        }).catch( () => {
+            loading.classList.remove("modal__overlay--visible")
+            alert(
+                "This service is temporarily down. Please contact me diretly at e0543918@u.nus.edu."
+            )
+        })
+}
